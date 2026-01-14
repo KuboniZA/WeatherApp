dd4248759745a95e3318ac2aea03abd2928d1251
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-01-14 17:43:13 +0200

    Update code to use Flask instead of Fast API.

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from flask import Flask, jsonify
 import requests
 from dotenv import load_dotenv
 import os
@@ -8,18 +8,34 @@ load_dotenv()
 API_KEY = os.getenv("WEATHER_API_KEY")
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
-app = FastAPI()
+if not API_KEY:
+    raise ValueError("WEATHER_API_KEY is not set in .env")
 
-@app.get("/weather")
+app = Flask(__name__)
+
+@app.route("/weather")
 def get_weather(city:str):
-    params = {
-        "q": city,
-        "appid": API_KEY,
-        "units": "metric"
-    }
+    city = requests.args.get("city")
+    if not city:
+        return jsonify({"error": "Please provide a city parameter, e.g., /weather?city=London"}), 400
+    try:
+        params = {
+            "q": city,
+            "appid": API_KEY,
+            "units": "metric"
+        }
 
-    response = requests.get(BASE_URL, params=params)
+        response = requests.get(BASE_URL, params=params, timeout=8)
+        data = response.json()
 
-    if response.status_code !=200:
-        return {"error": "City not found."}
-    return response.json()
+        if response.status_code !=200:
+            return jsonify({
+                "error": data.get("message", "City not found.")
+            }), response.status_code
+        
+        return jsonify(data)
+    except requests.exceptions.RequestException:
+        return jsonify({"error": "Failed to connect to weather service"}), 503
+
+if __name__ == "__main__":
+    app.run(debug=True)

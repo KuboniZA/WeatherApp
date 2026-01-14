075b89b1a56a84e777e2ec28bdc94fe51f040f56
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-01-14 16:50:51 +0200

    Cleanup typescript.

--- WeatherAppFE/src/components/WeatherView.vue
+++ WeatherAppFE/src/components/WeatherView.vue
@@ -2,19 +2,30 @@
 import { ref } from 'vue';
 import axios from 'axios';
 
-const city = ref('');
-const weather = ref (null);
-const error = ref('')
+interface weatherResponse {
+  name: string
+  main: {
+    temp: number
+  }
+  weather : {
+    description: string
+  }[]
+}
+
+const city = ref<string>('');
+const weather = ref<weatherResponse | null>(null);
+const error = ref<string>('')
 
 const getWeather = async () => {
   try {
     error.value = '';
-    const res = await axios.get(
-      `http://127.0.0.1:8000/weather?city=${city.value}`
+    const res = await axios.get<weatherResponse>(
+      `http://127.0.0.1:8000/weather?city=${encodeURIComponent(city.value)}`
     );
     weather.value =res.data;
   } catch {
     error.value = 'City not found';
+    weather.value = null
   }
 };
 </script>
@@ -29,8 +40,8 @@ const getWeather = async () => {
 
   <div v-if="weather">
     <h2>{{ weather.name }}</h2>
-    <p>Temperature: {{ weather.main.temp }} &deg;C</p>
-    <p>Condition: {{ weather.weather[0].description }}</p>
+    <p>Temperature: {{ weather?.main?.temp }} &deg;C</p>
+    <p>Condition: {{ weather?.weather[0]?.description }}</p>
   </div>
 </div>
 </template>

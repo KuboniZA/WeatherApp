91e27560b582be8c47cd7d849fa3f915e76f3121
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-01-15 15:01:38 +0200

    Backend is working, I think.

--- Backend/main.py
+++ Backend/main.py
@@ -1,21 +1,23 @@
-from flask import Flask, jsonify
-import requests
+from flask import Flask, jsonify, request
+# import requests
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+API_KEY = os.getenv("WEATHER_API_KEY")
+
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+if not API_KEY:
+    raise ValueError("WEATHER_API_KEY is not set in .env")
 
 app = Flask(__name__)
 
 @app.route("/weather")
-def get_weather(city:str):
+def get_weather():
     # return jsonify(message="Hello from the backend!")
 
-    load_dotenv()
-    API_KEY = os.getenv("WEATHER_API_KEY")
-    city = requests.args.get()
-    BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
-    if not API_KEY:
-        raise ValueError("WEATHER_API_KEY is not set in .env")
+    city = request.args.get("city")
+    
     if not city:
         return jsonify({"error": "Please provide a city parameter, e.g., /weather?city=London"}), 400
     
@@ -26,7 +28,7 @@ def get_weather(city:str):
             "units": "metric"
         }
 
-        response = requests.get(BASE_URL, params=params, timeout=8)
+        response = request.get(BASE_URL, params=params, timeout=8)
         data = response.json()
 
         if response.status_code !=200:
@@ -35,7 +37,7 @@ def get_weather(city:str):
             }), response.status_code
         
         return jsonify(data)
-    except requests.exceptions.RequestException:
+    except request.exceptions.RequestException:
         return jsonify({"error": "Failed to connect to weather service"}), 503
 
 if __name__ == "__main__":
